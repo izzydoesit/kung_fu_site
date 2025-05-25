60226cd0ca747f337497ced298cbe78aefe213e3
Izzy Matos <15269860+izzydoesit@users.noreply.github.com>
2025-05-24 23:02:34 -0400

    Add contact template w/ link, contact method to views, url path

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import about_view, home_view
+from .views import about_view, contact_view, home_view
 
 urlpatterns = [
     path("", home_view, name="home"),
     path("about/", about_view, name="about"),
+    path("contact/", contact_view, name="contact"),
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,7 @@ def home_view(request):
 
 def about_view(request):
     return render(request, "about.html")
+
+
+def contact_view(request):
+    return render(request, "contact.html")
